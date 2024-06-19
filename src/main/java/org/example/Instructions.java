45e7abed84bb3b0d81559f37993278846239fa56
waengs <144982285+waengs@users.noreply.github.com>
2024-06-19 19:04:43 +0700

    Update Instructions.java

--- src/main/java/org/example/Instructions.java
+++ src/main/java/org/example/Instructions.java
@@ -1,8 +1,8 @@
 package org.example;
 
+// interface used for song and scale
 public interface Instructions {
     String instructions();
     String[] getNoteNames();
     float[] getNotePitches();
 }
-
